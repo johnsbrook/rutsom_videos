8754736cdcf39bace2e7a341649e3d54710a131b
Ivan Zapata <falenyo@gmail.com>
2020-10-06 21:39:52 -0400

    created list tags and format

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const videosSep = [
+    {
+        "url": "3HE2jxHJ2KY",
+    },
+    {
+        "url": "b6aMqbuwUhs",
+    }
+]
+
+mapJuly();
+function mapJuly() {
+    videosSep.map((v) => {
+        let i = createNote('iframe');
+        
+    })
+}
+
+  /* <iframe width="560" height="315" src="https://www.youtube.com/embed/3HE2jxHJ2KY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
+
